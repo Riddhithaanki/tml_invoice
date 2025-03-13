9ffcbcdb9d91058e4c7510dab869caad100ad1b8
devamjoshi <devamjoshi8@gmail.com>
2025-03-13 11:08:41 +0530

    Change Logo + Added Dashboard New Design

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Admin Panel')</title>
+    <link rel="icon" type="image/x-icon" href="{{ url('img/favicon.ico') }}" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
