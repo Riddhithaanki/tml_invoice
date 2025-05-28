53eefbaccca9ce22710d7be358368a848d3ca8f8
devam_joshi <devamjoshi8@gmail.com>
2025-05-28 23:49:41 +0530

    Sidebar Changed

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -65,13 +65,14 @@
             </a>
         </li>
 
+        <li class="menu-item {{ Request::routeIs('invoice.differences') ? 'active' : '' }}">
+            <a href="{{ route('invoice.differences') }}" class="menu-link">
+                <i class="menu-icon tf-icons mdi mdi-file-document-outline"></i>
+                <div>Invoice Comparison</div>
+            </a>
+        </li>
+
         @if (session('roleId') != 8)
-            <li class="menu-item {{ Request::routeIs('invoice.differences') ? 'active' : '' }}">
-                <a href="{{ route('invoice.differences') }}" class="menu-link">
-                    <i class="menu-icon tf-icons mdi mdi-file-document-outline"></i>
-                    <div>Invoice Comparison</div>
-                </a>
-            </li>
             <!-- Users -->
             <li class="menu-item {{ Request::routeIs('users.*') ? 'active' : '' }}">
                 <a href="{{ route('users.list') }}" class="menu-link">
